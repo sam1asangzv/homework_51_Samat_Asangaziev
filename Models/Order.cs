89cf93a48bd02a297cd5d11cc878a4641e835c4f
Asangaziev Samat <sam1asangzv@gmail.com>
2026-08-21 19:03:08 +0600

    Translate interface to Russian

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,12 +1,22 @@
-﻿namespace WebApplication1.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models;
 
 public class Order
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Address { get; set; }
-    public string ContactPhone { get; set; }
-    
+
+    [Display(Name = "Имя покупателя")]
+    public string Name { get; set; } = "";
+
+    [Display(Name = "Адрес доставки")]
+    public string Address { get; set; } = "";
+
+    [Display(Name = "Номер телефона")]
+    public string ContactPhone { get; set; } = "";
+
     public int PhoneId { get; set; }
-    public Phone Phone { get; set; }
+
+    [Display(Name = "Смартфон")]
+    public Phone Phone { get; set; } = null!;
 }
--- Models/Phone.cs
+++ Models/Phone.cs
@@ -1,16 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApplication1.Models;
 
 public class Phone
 {
     public int Id { get; set; }
 
+    [Display(Name = "Название")]
     public string Name { get; set; } = "";
 
+    [Display(Name = "Производитель")]
     public string Company { get; set; } = "";
 
+    [Display(Name = "Цена в долларах")]
     public int Price { get; set; }
 
+    [Display(Name = "Описание")]
     public string Description { get; set; } = "";
 
+    [Display(Name = "Ссылка на фото")]
     public string ImageUrl { get; set; } = "";
 }
